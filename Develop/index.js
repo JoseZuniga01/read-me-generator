6d10c253ba72a77dbaf41c872669ce731c7ca4ea
Jose Zuniga <josezuniga@Joses-Macbook.local>
2022-06-02 22:49:36 -0400

    starter code

---
+++ Develop/index.js
@@ -0,0 +1,123 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer'); 
+const fs = require('fs'); 
+const generatePage = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+    // will use inquierer to bring up Qs
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+                return false; 
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: 'What is in your table of contents for this project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your Table of Contents!');
+                return false; 
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the installation instructions for your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the installation instructions for your project!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a usage description!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What license does your porject have?',
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project license!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'tests', 
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'How can the user contribute to the repo?'
+    }
+]);
+};
+
+// TODO: Create a function to write README file
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("You have succesfully created your README!")
+        }
+    })
+}; 
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
